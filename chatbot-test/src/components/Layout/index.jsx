00aad279b0b2bcf542810dc56fcddb00d9ad0424
joy0316webiee <joy0316webiee@gmail.com>
2019-10-31 23:17:30 +0100

    minor css fix

--- chatbot-test/src/components/Layout/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './styles.scss';
-
-const Layout = ({ children }) => (
-  <div className="layout__wrapper">{children}</div>
-);
-
-export default Layout;
--- chatbot-test/src/config/_api.js
+++
@@ -1,1 +0,0 @@
-export const apiUrl = 'http://www.mocky.io/v2/5cee37a5300000253a6e99af';
